5208bc39fe7c4fb11abcc1f8933f44491b6e6424
Miguel Ciulog <angel.ciulog7@gmail.com>
2022-03-13 22:35:10 -0700

    Navegacion en Películas

---
+++ app/src/main/java/ciulog/miguel/peliculas/PeliculaActivity.kt
@@ -0,0 +1,11 @@
+package ciulog.miguel.peliculas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PeliculaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pelicula)
+    }
+}
